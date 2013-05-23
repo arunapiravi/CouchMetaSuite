c6df2f7ed9feb06f330cf1c6f0ee88eab4552ac4
abhinavdangeti <abhinav@couchbase.com>
2013-05-23 13:30:43 -0700

    Updating Verification module

--- Verification.java
+++ Verification.java
@@ -1,4 +1,5 @@
 import java.util.Iterator;
+import java.util.Map;
 
 import com.couchbase.client.CouchbaseMetaClient;
 
@@ -7,8 +8,24 @@ public class Verification {
 
 	public void comparison(Stronghold sh, CouchbaseMetaClient sclient, CouchbaseMetaClient dclient) {
 		populate_destTable(sh, dclient);
+		
+		int BADflag = 0;
+		for (Map.Entry<String, Hashstructure> htEntries : sh.sourceContent.entrySet()) {
+			if (sh.destinationContent.containsKey(htEntries.getKey())) {
+				if (!(sh.destinationContent.get(htEntries.getKey()).readdata().equals(htEntries.getValue().readdata()))
+						&& (sh.destinationContent.get(htEntries.getKey()).readmetadata().equals(htEntries.getValue().readmetadata()))) {
+					BADflag = 1;
+					break;
+				}
+			}
+		}
+		if (BADflag == 1) {
+			System.out.println("VERIFICATION FAILED");
+		} else {
+			System.out.println("PASSED VERIFICATION");
+		}
 	}
-	
+
 	public void populate_destTable(Stronghold sh, CouchbaseMetaClient dclient) {
 		Iterator it = sh.destinationContent.entrySet().iterator();
 		while (it.hasNext()) {
